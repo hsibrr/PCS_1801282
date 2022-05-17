76f457fb0d758838c1a3ac6627d1f400692511b1
Srah <105329869+hsibrr@users.noreply.github.com>
2022-05-16 21:18:32 -0500

    Delete Redes4.psm1

--- Redes4.psm1
+++
@@ -1,4 +0,0 @@
-function Agregar-ReglasBloqueo{ 
-	$puerto = Read-Host -Prompt "Cuál puerto quieres bloquear?" 
-	New-NetFirewallRule -DisplayName "Puerto-Entrada-$puerto" -Profile "Public" -Direction Inbound -Action Block -Protocol TCP -LocalPort $puerto 
-}
